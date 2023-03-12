788319c00f17946a5f89068201b8bc7a8e10c61b
Franz Li <92065397+FranzLi347@users.noreply.github.com>
2023-03-12 09:45:50 +0800

    Update apiauth.lua

--- apiauth.lua
+++ apiauth.lua
@@ -1,21 +1,30 @@
 -- require
 local cjson = require "cjson"
+local sha = require "sha2"
 -- golbal variables
 local uri = ngx.var.uri
 local headers = ngx.req.get_headers()
-local access_key = headers["accessKey"]
-local sign = headers["sign"]
-local timestamp = headers["timestamp"]
-local nouce = headers['nouce']
-local sha1 = require("sha1")
-
+-- sign data form headers
+local signDt = {
+  access_key = headers["accessKey"],
+  sign = headers["sign"],
+  timestamp = headers["timestamp"],
+  nouce = headers['nouce']
+}
+local props = {
+  host = "127.0.0.1",
+  port = 3306,
+  database = "frzApi",
+  user = "root",
+  password = "123456"
+}
+local salt = "franzli"
 local function close_db( db )
     if not db then
       return
     end
     db:close()
-  end
-
+end
 local mysql = require("resty.mysql")
   -- 创建实例
 local db, err = mysql:new()
@@ -25,25 +34,18 @@ if not db then
 end
   -- 设置超时时间(毫秒)
 db:set_timeout(5000)
-
-local props = {
-    host = "127.0.0.1",
-    port = 3306,
-    database = "frzApi",
-    user = "root",
-    password = "123456"
-}
-
 local res, err, errno, sqlstate = db:connect(props)
-
 if not res then
     ngx.say("connect to mysql error : ", err, " , errno : ", errno, " , sqlstate : ", sqlstate)
     return close_db(db)
 end
 
-local skData = {}
+local function auth_error(tag)
+  ngx.status = ngx.HTTP_UNAUTHORIZED
+  ngx.say(cjson.encode({msg = "bad auth please check your req param" .. tag,code = 400}))
+end
 
-local function get_secertKey_by_accesskey()
+local function get_secertKey_by_accesskey(access_key) 
     local quoted= ngx.quote_sql_str(tostring(access_key))
     local sql = "SELECT * FROM key_info WHERE access_key = " .. quoted .."and is_delete = 0"
     res, err, errno, sqlstate = db:query(sql)
@@ -51,33 +53,68 @@ local function get_secertKey_by_accesskey()
         ngx.say("database Error \n" .. err)
         close_db(db)
         return
-     end
+    end
     for i, row in ipairs(res) do
         for name, value in pairs(row) do
-          skData[name] = value
+          if name == "secert_key" then
+            return value
+          end
+
         end
     end
 end
 
 
-local function get_sign(serverSk)
-  return sha1(serverSk .. access_key)
+local function router(uri)
+  local quoted= ngx.quote_sql_str(tostring(uri))
+  local sql = "SELECT * FROM uri_mapping WHERE source = " .. quoted .."and is_delete = 0"
+  res, err, errno, sqlstate = db:query(sql)
+  if not res then
+    ngx.say("database Error \n" .. err)
+    close_db(db)
+    return
+  end
+  for i, row in ipairs(res) do
+    for name, value in pairs(row) do
+       if name == "taget" then
+          ngx.var.upstream = value
+        return
+      end
+    end
+  end
 end
 
-local function auth()
-    
+
+local function get_sign(access_key,timestamp,nouce)
+  local sk = get_secertKey_by_accesskey(access_key)
+  if sk == nil then
+    auth_error(1)
+    return
+  end
+  --todo Add nose to Redis and verify nose times
+  return sha.sha1(access_key .. nouce .. salt .. sk)
+end
+
+
+local function main()
     -- validated the req parameter
-    -- if not access_key or not sign or not timestamp or not nouce then
-    --     ngx.status = ngx.HTTP_UNAUTHORIZED
-    --     ngx.say(cjson.encode({msg = "bad auth please check your req param",code = 400}))
-    --     return
-    -- end
-    get_secertKey_by_accesskey()
-    -- todo validated by sign
-    if skData.secert_key == "123321" and skData.access_key == "123321" then
-      local service = ngx.var.service
-      local upstream = "http://v.api.aa1.cn/api/yiyan/index.php"
-      ngx.var.upstream = upstream
+    if not signDt.access_key
+    or not signDt.sign
+    or not signDt.timestamp
+    or not signDt.nouce
+    or os.time() - signDt.timestamp > 60000
+    then
+      auth_error(2)
+      return
+    end
+    local server_sign = tostring(get_sign(signDt.access_key,signDt.timestamp,signDt.nouce))
+
+    if server_sign == signDt.sign then
+      router(uri)
+      return
+    else
+      auth_error(3)
+      return
     end
 end
 
@@ -85,6 +122,5 @@ end
 
 
 -- main function
--- default 
-ngx.header.content_type = "application/json"
-auth()
+main()
+
